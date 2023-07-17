af205534a3f11f96109f384ce50f6eeb62c4b634
Faith-qa <atienofaith12@gmail.com>
2023-07-17 13:56:41 +0000

    starting out on c again

---
+++ alx-low_level_programming/0x00-hello_world/main.s
@@ -0,0 +1,16 @@
+	.arch armv8-a
+	.file	"main.c"
+	.text
+	.align	2
+	.global	main
+	.type	main, %function
+main:
+.LFB0:
+	.cfi_startproc
+	mov	w0, 0
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
